af84a1bcf1c8db41bd8cb6d783b5ca58364f40fa
bosciu <bosciu@icloud.com>
2021-05-28 14:35:49 +0200

    Completamento componente Jumbotron

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
 	<div id="app">
 		<img alt="Vue logo" src="./assets/logo.png" />
 		<Title />
+		<Hero />
 	</div>
 </template>
 
 <script>
+import Hero from "./components/Hero.vue";
 import Title from "./components/Title";
 
 export default {
 	name: "App",
 	components: {
-		Title
+		Title,
+		Hero
 	}
 };
 </script>
@@ -22,7 +25,12 @@ export default {
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
-	color: #2c3e50;
+	position: relative;
 	margin-top: 60px;
+}
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
 }
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,47 @@
+<template>
+	<div id="container">
+		<div id="text-container">
+			<h2>Signori e signore, ecco a voi i famosi "Componenti"!</h2>
+			<p>
+				I componenti sono utili per spezzettare la complessità di
+				un'applicazione in tanti piccoli pezzi, così da renderli
+				riutilizzabili, e facilmente manutenibili. <br />
+				<br />
+				Cosa aspetti? Approfondisci subito!
+			</p>
+		</div>
+		<img src="../assets/components.png" alt="" />
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Hero"
+};
+</script>
+
+<style scoped>
+#container {
+	position: relative;
+	width: 80%;
+	margin: auto;
+	height: 500px;
+}
+img {
+	position: absolute;
+	width: 60%;
+	right: 0;
+	top: 50%;
+	transform: translateY(-50%);
+}
+#text-container {
+	width: 40%;
+	height: 100%;
+	padding: 60px 30px;
+}
+p {
+	margin-top: 40px;
+	font-size: 25px;
+	text-align: left;
+}
+</style>
